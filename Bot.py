35a9fad541053e456499d3b5bf22b5caafcbfacc
HappyAcidRain <Sashaklivsovpechka@gmail.com>
2023-02-01 22:35:56 +0300

    GIGA CHAD FIRST UPDATE

--- Bot.py
+++ Bot.py
@@ -1,23 +1,29 @@
 import telebot
 import time
 
-
 bot = telebot.TeleBot("5953159365:AAHbB40ATCR-Zw6nMBKEphiam62l9ejIpIY")
+
+pics_value = 242
 
 
 @bot.message_handler(commands=['startSending'])
 def start(message):
-    pics_value = 0
+    steps = 0
 
-    while pics_value <= 6:
-
+    while steps <= pics_value:
         # отправляем картинку
-        cat_photo = open(f"ParserPics/images{pics_value}.jpg", 'rb')
+        cat_photo = open(f"ParserPics/images{steps}.jpg", 'rb')
         bot.send_photo(message.chat.id, cat_photo)
 
         # задержка + переключение картинки
-        pics_value += 1
-        time.sleep(14400)
+        steps += 1
+        time.sleep(2)
+
+
+@bot.message_handler(commands=['globalStop'])
+def stop(message):
+    bot.send_message(message.chat.id, "Бот остановлен!")
+    bot.stop_bot()
 
 
 bot.polling(none_stop=True)
